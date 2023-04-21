17c2c2ab537d717cd70da16d593443c222cbf50e
CPsymmetry <73593071+CPsymmetry@users.noreply.github.com>
2023-04-21 11:22:14 +0100

    Update Energy resolution.py

--- Energy resolution.py
+++ Energy resolution.py
@@ -8,6 +8,12 @@ Created on Thu Mar  9 15:55:30 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
+from scipy.stats import pareto
+
+'''
+Errors on tagged neutrino as a function of energy
+'''
 
 kaon_m = 0.493677 #kaon mass in GeV with an error of 0.013 Mev
 muon_m = 0.105658 #muon mass in GeV with an error of 0.0000023 Mev
@@ -15,47 +21,56 @@ muon_m = 0.105658 #muon mass in GeV with an error of 0.0000023 Mev
 kaon_p = 75 #kaon momentum value with error of 1.6%
 kaon_E = np.sqrt(kaon_m**2 + kaon_p**2)
 
-gtk_res = 0.002*kaon_p #momentum resolution of GTK
-#straw_res = 0.003000416638*(muon_p**2)
+kaon_res_p = 0.002*kaon_p #momentum resolution of GTK
 
-kaon_res = gtk_res * kaon_p/kaon_E
-#muon_res = straw_res*muon_p/(np.sqrt(muon_p**2 + muon_m**2))
-
-#neutrino_E_res = np.sqrt(kaon_res**2 + muon_res**2)
-
-neutrino_E = np.loadtxt("neutrino_energies.txt")
+neutrino_E = np.loadtxt("neutrino_energy_dist.txt")
 neutrino_E = np.round(neutrino_E,3)
 neutrino_E = np.unique(neutrino_E, return_counts=False)
 
-frac = neutrino_E /kaon_E
-
 muon_E = kaon_E - neutrino_E  #an array of the muon energies 
+muon_p = np.sqrt(muon_E**2-muon_m**2)
+muon_res_p = np.sqrt(0.003**2+(0.00005*muon_p)**2)*muon_p
 
-neutrino_res = np.sqrt(kaon_res**2 + ((0.003000416638*(muon_E**2 - muon_m**2)**(3/2))/(muon_E))**2)/neutrino_E  #an array of sigma_E/E of the neutrino energies 
+neutrino_res = np.sqrt((kaon_p*kaon_res_p/kaon_E)**2+(muon_p*muon_res_p/muon_E)**2)/neutrino_E
 
 x_values = neutrino_E
 y_values = neutrino_res
-log = np.log(y_values)
-x = np.linspace(min(x_values),max(x_values),100)
+x = np.linspace(np.min(x_values),np.max(x_values),100)
 
-fit = np.polyfit(x_values,log,1) #linear fit for log plot
-fit = np.flip(fit)
-
-plt.figure()
-plt.title("Logarithmic Energy resolution on the tagged neutrino")
-plt.plot(x_values,log, 'b', label = "Uncertainty in NA62")
-plt.plot(x, fit[0] + fit[1]*x, 'k', label = "Linear fit")
-plt.xlabel('Energy of the neutrinos (GeV)')
-plt.ylabel('log($\sigma$E/E)')
-plt.legend()
-plt.show()
+popt, pcov = opt.curve_fit(lambda x,a,b,c: a/x**b + c, x_values, y_values)
 
 plt.figure()
 plt.title("Energy resolution on the tagged neutrino")
 plt.plot(x_values, y_values, label ="Uncertainty in NA62")
-plt.plot(x, np.exp(fit[0]) * np.exp(fit[1]*x), 'k', label = "Exponential fit")
+plt.plot(x,popt[0]/x**popt[1] + popt[2],'k--',label = "Power law fit")
 plt.xlabel('Energy of the neutrinos (GeV)')
-plt.ylabel('$\sigma$E/E')
+plt.ylabel('$\delta$(E)/E')
 plt.legend()
 plt.show()
 
+print(f"The fractional error on the tagged neutrinos has the form a/E**b + c where [a,b,c] are {popt} respectively")
+
+'''
+Errors on cross-section from MINOS data as a function of energy
+'''
+
+x = np.linspace(3,60,100)
+x2 = np.array([3.48,4.45,5.89,7.97,10.45,13.43,16.42,19.87,23.88,27.89,32.81,38.87,45.77])#MINOS Energy values
+y2 = np.array([0.748,0.711,0.708,0.722,0.699,0.691,0.708,0.689,0.683,0.686,0.675,0.675,0.676])#MINOS cross-section values
+y2_err = np.array([0.061,0.033,0.032,0.045,0.043,0.028,0.020,0.016,0.015,0.016,0.016,0.018,0.019])#MINOS cross-section errors
+
+err = y2_err/y2 #fractional error
+
+b = 0.3
+rv = pareto(b)  #Fitting inverse power law to errors
+
+plt.figure()
+plt.plot(x2,err,'bo',label = "Fractional errors") #MINOS erros
+plt.plot(x, rv.pdf(x) + np.min(err), 'k-', lw=2, label = 'Power law fit') #Trend line
+plt.legend()
+plt.title(r"Errors from MINOS for ${}^{56} \rm Fe$")
+plt.ylabel(r'Fractional error of ${}^{56} \rm Fe$')
+plt.xlabel('Energy (GeV)')
+plt.show()
+
+print(f"The fractional error on the cross-section values has the form {b}*E**-{b} + {np.min(err)}")
